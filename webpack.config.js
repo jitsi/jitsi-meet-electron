994a605ce0b9cbf31ca74431f925d8f43b7bbea3
Saúl Ibarra Corretgé <s@saghul.net>
2018-06-16 23:22:19 +0200

    Add extra development utilities
    - devtron
    - electron-react-devtools

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,8 @@ const commonConfig = {
         ]
     },
     externals: [ {
-        'jitsi-meet-electron-utils': 'require(\'jitsi-meet-electron-utils\')'
+        'jitsi-meet-electron-utils': 'require(\'jitsi-meet-electron-utils\')',
+        'electron-debug': 'require(\'electron-debug\')'
     } ],
     resolve: {
         modules: [

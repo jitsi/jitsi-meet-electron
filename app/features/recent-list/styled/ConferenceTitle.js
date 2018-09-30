24e2910e67871a9b9bb5a8f5215d4ae6d0d664c2
Saúl Ibarra Corretgé <saghul@gmail.com>
2018-09-30 21:16:26 +0200

    Fixed styled components warning
    This is the new syntax for extending components. See:
    https://github.com/styled-components/styled-components/issues/1546

--- app/features/recent-list/styled/ConferenceTitle.js
+++ app/features/recent-list/styled/ConferenceTitle.js
@@ -1,8 +1,10 @@
 // @flow
 
+import styled from 'styled-components';
+
 import { default as TruncatedText } from './TruncatedText';
 
-export default TruncatedText.extend`
+export default styled(TruncatedText)`
     font-size: 1.1em;
     font-weight: bold;
 `;

db2519e595c910773b7392d583be82de87d55dee
Christian Pohle <pohle@pohle.com>
2020-04-12 19:36:25 +0200

    Changed URLs

--- app/features/config/index.js
+++ app/features/config/index.js
@@ -3,30 +3,30 @@ export default {
     /**
      * The URL with extra information about the app / service.
      */
-    aboutURL: 'https://jitsi.org/what-is-jitsi/',
+    aboutURL: 'https://support.pohle.net/display/HC/Conference+Server/',
 
     /**
      * Application name.
      */
-    appName: 'Jitsi Meet',
+    appName: 'Pohle Conference Server',
 
     /**
      * The default server URL of Jitsi Meet Deployment that will be used.
      */
-    defaultServerURL: 'https://meet.jit.si',
+    defaultServerURL: 'https://conference.pohle.net',
 
     /**
      * URL to send feedback.
      */
-    feedbackURL: 'mailto:support@jitsi.org',
+    feedbackURL: 'mailto:support@pohle.net',
 
     /**
      * The URL of Privacy Policy Page.
      */
-    privacyPolicyURL: 'https://jitsi.org/meet/privacy',
+    privacyPolicyURL: 'https://support.pohle.net/display/HC/Conference+Server',
 
     /**
      * The URL of Terms and Conditions Page.
      */
-    termsAndConditionsURL: 'https://jitsi.org/meet/terms'
+    termsAndConditionsURL: 'https://support.pohle.net/display/HC/Conference+Server'
 };

d2f49288f13bfd9590d7968909b096353e87231b
Saúl Ibarra Corretgé <s@saghul.net>
2020-05-19 11:43:55 +0200

    Add randdom room name generation

---
+++ app/features/welcome/styled/FieldWrapper.js
@@ -0,0 +1,10 @@
+// @flow
+
+import styled from 'styled-components';
+
+export default styled.div`
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    magin: auto;
+`;
---
+++ app/features/welcome/styled/Label.js
@@ -0,0 +1,8 @@
+// @flow
+
+import styled from 'styled-components';
+
+export default styled.span`
+    color: white;
+    font-weight: bold;
+`;

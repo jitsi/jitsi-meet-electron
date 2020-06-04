962470d97e8faefa5728ff4c324ce387cfd456e0
Saúl Ibarra Corretgé <s@saghul.net>
2020-06-04 09:54:18 +0200

    Restrict URLs which can be opened in an external browser

--- app/preload/preload.js
+++ app/preload/preload.js
@@ -1,13 +1,37 @@
 const createElectronStorage = require('redux-persist-electron-storage');
 const { shell } = require('electron');
 const os = require('os');
+const url = require('url');
 
 const jitsiMeetElectronUtils = require('jitsi-meet-electron-utils');
+
+
+const protocolRegex = /^https?:/i;
+
+/**
+ * Opens the given link in an external browser.
+ *
+ * @param {string} link - The link (URL) that should be opened in the external browser.
+ * @returns {void}
+ */
+function openExternalLink(link) {
+    let u;
+
+    try {
+        u = url.parse(link);
+    } catch (e) {
+        return;
+    }
+
+    if (protocolRegex.test(u.protocol)) {
+        shell.openExternal(link);
+    }
+}
+
 
 window.jitsiNodeAPI = {
     createElectronStorage,
     osUserInfo: os.userInfo,
-    shellOpenExternal: shell.openExternal,
+    openExternalLink,
     jitsiMeetElectronUtils
 };
-

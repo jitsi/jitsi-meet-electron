759b9a005b62069fb41d9fb2120bf21807b9b097
Saúl Ibarra Corretgé <s@saghul.net>
2020-06-04 15:35:45 +0200

    Reorganize settings drawer

--- app/features/onboarding/components/index.js
+++ app/features/onboarding/components/index.js
@@ -4,10 +4,7 @@ export { default as NameSettingSpotlight } from './NameSettingSpotlight';
 export { default as Onboarding } from './Onboarding';
 export { default as OnboardingModal } from './OnboardingModal';
 export { default as ServerSettingSpotlight } from './ServerSettingSpotlight';
+export { default as ServerTimeoutSpotlight } from './ServerTimeoutSpotlight';
 export { default as SettingsDrawerSpotlight } from './SettingsDrawerSpotlight';
-export {
-    default as StartMutedTogglesSpotlight
-} from './StartMutedTogglesSpotlight';
-export {
-    default as AlwaysOnTopWindowSpotlight
-} from './AlwaysOnTopWindowSpotlight';
+export { default as StartMutedTogglesSpotlight } from './StartMutedTogglesSpotlight';
+export { default as AlwaysOnTopWindowSpotlight } from './AlwaysOnTopWindowSpotlight';
--- app/features/settings/styled/SettingsContainer.js
+++ app/features/settings/styled/SettingsContainer.js
@@ -4,5 +4,6 @@ import styled from 'styled-components';
 
 export default styled.div`
     margin: 0 auto;
+    margin-bottom: 50px;
     width: 70%;
 `;

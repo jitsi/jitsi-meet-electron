4f1e16edf4fb040649662584fefdd494650dba17
Freddy Tuxworth <freddytuxworth@gmail.com>
2020-06-25 12:12:14 +0100

    i18n for time strings with moment

--- app/i18n/index.js
+++ app/i18n/index.js
@@ -1,19 +1,24 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import moment from 'moment';
 
 const languages = {
     en: { translation: require('./lang/en.json') }
 };
 
+const detectedLocale = window.jitsiNodeAPI.getLocale();
+
 i18n
     .use(initReactI18next)
     .init({
         resources: languages,
-        lng: window.jitsiNodeAPI.getLocale(),
+        lng: detectedLocale,
         fallbackLng: 'en',
         interpolation: {
             escapeValue: false // not needed for react as it escapes by default
         }
     });
+
+moment.locale(detectedLocale);
 
 export default i18n;

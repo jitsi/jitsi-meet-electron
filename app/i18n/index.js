db1680a561d75c71808544280ceda7db08fd563e
Annika Wickert <annika.wickert@exaring.de>
2020-06-29 10:55:05 +0200

    Add french, spanish and russian

--- app/i18n/index.js
+++ app/i18n/index.js
@@ -5,8 +5,10 @@ import moment from 'moment';
 const languages = {
     de: { translation: require('./lang/de.json') },
     en: { translation: require('./lang/en.json') },
+    es: { translation: require('./lang/es.json') },
     fr: { translation: require('./lang/fr.json') },
-    it: { translation: require('./lang/it.json') }
+    it: { translation: require('./lang/it.json') },
+    ru: { translation: require('./lang/ru.json') }
 };
 
 const detectedLocale = window.jitsiNodeAPI.getLocale();

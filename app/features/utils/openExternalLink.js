ca1eb702507fdc4400fe21c905a9f85702f92a14
Saúl Ibarra Corretgé <s@saghul.net>
2020-06-30 11:16:21 +0200

    Share logic for opening external links

---
+++ app/features/utils/openExternalLink.js
@@ -0,0 +1,25 @@
+const { shell } = require('electron');
+const url = require('url');
+
+
+const protocolRegex = /^https?:/i;
+
+/**
+ * Opens the given link in an external browser.
+ *
+ * @param {string} link - The link (URL) that should be opened in the external browser.
+ * @returns {void}
+ */
+export function openExternalLink(link) {
+    let u;
+
+    try {
+        u = url.parse(link);
+    } catch (e) {
+        return;
+    }
+
+    if (protocolRegex.test(u.protocol)) {
+        shell.openExternal(link);
+    }
+}

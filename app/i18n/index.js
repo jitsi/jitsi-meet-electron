a2bb37f455d3e9a6d43cdbd02026b2c9edc9ef1a
Christoph Settgast <csett86@web.de>
2021-01-03 23:05:07 +0100

    Revert "Change fallback lang to de"
    This reverts commit df04b9c983a4e748f19b2ff82d72165068a8a0ca.
    
    Issue was https://github.com/electron/electron/issues/26612, so default lang was irrelevant

--- app/i18n/index.js
+++ app/i18n/index.js
@@ -19,7 +19,7 @@ i18n
     .init({
         resources: languages,
         lng: detectedLocale,
-        fallbackLng: 'de',
+        fallbackLng: 'en',
         interpolation: {
             escapeValue: false // not needed for react as it escapes by default
         }

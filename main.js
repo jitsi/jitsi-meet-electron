59a3c12b081a77b4323d83a19393e1192bad0d9b
Christoph Settgast <csett86@web.de>
2021-03-11 21:28:31 +0100

    Enhancements to make it more Mac App Store (mas) compatible
    - disable the autoupdater if running as mas (was not working anyway, just logging an error on every start)
    - replace check via app.requestSingleInstanceLock() with LSMultipleInstancesProhibited in Info.plist
      due to https://github.com/electron/electron/issues/15958
    - Quit the app also when all windows are closed to conform to macOS Human Interface Guidelines
      Comments from review:
      If the application is a single-window app, it might be appropriate to save data and quit the app when the main window is closed.
    - "asarUnpack": "**/*.node" to also sign the native addons when packaging
    - add the required mas-specific entitlements which include the app-sandbox key
    
    Signed-off-by: Christoph Settgast <csett86@web.de>

--- main.js
+++ main.js
@@ -161,7 +161,9 @@ function createJitsiMeetWindow() {
     setApplicationMenu();
 
     // Check for Updates.
-    autoUpdater.checkForUpdatesAndNotify();
+    if (!process.mas) {
+        autoUpdater.checkForUpdatesAndNotify();
+    }
 
     // Load the previous window state with fallback to defaults.
     const windowState = windowStateKeeper({
@@ -278,8 +280,9 @@ function handleProtocolCall(fullProtocolCall) {
 
 /**
  * Force Single Instance Application.
+ * Handle this on darwin via LSMultipleInstancesProhibited in Info.plist as below does not work on MAS
  */
-const gotInstanceLock = app.requestSingleInstanceLock();
+const gotInstanceLock = process.platform === 'darwin' ? true : app.requestSingleInstanceLock();
 
 if (!gotInstanceLock) {
     app.quit();
@@ -333,10 +336,7 @@ app.on('second-instance', (event, commandLine) => {
 });
 
 app.on('window-all-closed', () => {
-    // Don't quit the application on macOS.
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+    app.quit();
 });
 
 // remove so we can register each time as we run the app.

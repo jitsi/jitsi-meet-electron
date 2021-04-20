5bfe4b313ea27d6a62c5e8231e875ffbb3a577e8
Besnik Bleta <besnik@programeshqip.org>
2021-04-20 12:42:22 +0300

    comma dancing again…

--- app/i18n/index.js
+++ app/i18n/index.js
@@ -12,7 +12,7 @@ const languages = {
     it: { translation: require('./lang/it.json') },
     pt: { translation: require('./lang/pt-br.json') },
     ru: { translation: require('./lang/ru.json') },
-    nl: { translation: require('./lang/nl.json') }
+    nl: { translation: require('./lang/nl.json') },
     sq: { translation: require('./lang/sq.json') }
 };
 

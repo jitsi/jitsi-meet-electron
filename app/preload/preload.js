f05dcc12769116130bd617c73d7ebcd4f0b3ce76
Christoph Settgast <csett86@web.de>
2021-05-22 11:44:10 +0200

    fix(mac): remove wifi stats as they cause hangs every 10 seconds
    The getWiFiStats causes screen freezes every 10 seconds for 2-4 seconds,
    thus temporarily remove them on mac.
    
    Closes: #595

--- app/preload/preload.js
+++ app/preload/preload.js
@@ -1,3 +1,5 @@
+/* global process */
+
 const createElectronStorage = require('redux-persist-electron-storage');
 const { ipcRenderer, remote } = require('electron');
 const os = require('os');
@@ -11,6 +13,7 @@ window.jitsiNodeAPI = {
     createElectronStorage,
     osUserInfo: os.userInfo,
     openExternalLink,
+    platform: process.platform,
     jitsiMeetElectronUtils,
     getLocale: remote.app.getLocale,
     ipc: {

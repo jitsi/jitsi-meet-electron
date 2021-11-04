f454c22273283f926de2c2006d9791e9bb57fe85
Saúl Ibarra Corretgé <s@saghul.net>
2021-11-04 11:43:45 +0100

    squash: notarize

---
+++ notarize.js
@@ -0,0 +1,22 @@
+const { notarize } = require('electron-notarize');
+const process = require('process');
+const pkgJson = require('./package.json');
+
+exports.default = async function notarizing(context) {
+    const { electronPlatformName, appOutDir } = context;
+
+    if (electronPlatformName !== 'darwin') {
+        return;
+    }
+
+    const appName = context.packager.appInfo.productFilename;
+
+    return await notarize({
+        tool: 'notarytool',
+        appBundleId: pkgJson.build.appId,
+        appPath: `${appOutDir}/${appName}.app`,
+        appleId: process.env.APPLE_ID,
+        appleIdPassword: process.env.APPLE_ID_PASSWORD,
+        teamId: process.env.TEAM_ID
+    });
+};

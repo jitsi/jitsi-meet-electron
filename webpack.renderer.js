f58f8c90ffa0fb6cb7a82d4c7854043a055f4e13
Kanishka Bansode <96020697+kb-0311@users.noreply.github.com>
2022-03-25 02:12:46 +0530

    chore(deps): migrate webpack 4 to 5 (#732)
    update babel to latest minor while at it

--- webpack.renderer.js
+++ webpack.renderer.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const webpack = require('webpack');
 const ELECTRON_VERSION = require('./package.json').devDependencies.electron;
 
 module.exports = {
@@ -15,6 +15,9 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './app/index.html'
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser'
         })
     ],
     output: {
@@ -81,7 +84,10 @@ module.exports = {
     resolve: {
         modules: [
             path.resolve('./node_modules')
-        ]
+        ],
+        alias: {
+            process: 'process/browser'
+        }
     }
 };
 

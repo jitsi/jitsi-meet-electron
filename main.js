jitsi/jitsi-meet-electron

commit 542c7f089a241487a0c7bc7905e21db93c5bb13b

chore: update jitsi-electron-sdk to 3.0.18

main.js
@@ -211,26 +211,30 @@ function createJitsiMeetWindow() {
         }
     };
 
+    const windowOpenHandler = ({ url, frameName }) => {
+        const target = getPopupTarget(url, frameName);
+
+        if (!target || target === 'browser') {
+            openExternalLink(url);
+        }
+
+        return { action: 'deny' };
+    };
+
     mainWindow = new BrowserWindow(options);
     windowState.manage(mainWindow);
     mainWindow.loadURL(indexURL);
 
+    mainWindow.webContents.setWindowOpenHandler(windowOpenHandler);
+
     initPopupsConfigurationMain(mainWindow);
-    setupAlwaysOnTopMain(mainWindow);
+    setupAlwaysOnTopMain(mainWindow, null, windowOpenHandler);
     setupPowerMonitorMain(mainWindow);
     setupScreenSharingMain(mainWindow, config.default.appName, pkgJson.build.appId);
     if (ENABLE_REMOTE_CONTROL) {
         new RemoteControlMain(mainWindow); // eslint-disable-line no-new
     }
 
-    mainWindow.webContents.on('new-window', (event, url, frameName) => {
-        const target = getPopupTarget(url, frameName);
-
-        if (!target || target === 'browser') {
-            event.preventDefault();
-            openExternalLink(url);
-        }
-    });
     mainWindow.on('closed', () => {
         mainWindow = null;
     });

commit 90cdde1bdb4022c682e36df114538e8bcabcf38c

fix: popups for Google & Dropbox
broken likely since 542c7f089a241487a0c7bc7905e21db93c5bb13b (#787)

main.js
@@ -217,6 +217,12 @@ function createJitsiMeetWindow() {
 
         if (!target || target === 'browser') {
             openExternalLink(url);
+
+            return { action: 'deny' };
+        }
+
+        if (target === 'electron') {
+            return { action: 'allow' };
         }
 
         return { action: 'deny' };
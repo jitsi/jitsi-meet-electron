cdda2e2eb31f43ba4a94a6293643e0fdae7b5b2c
shellheim <116480892+shellheim@users.noreply.github.com>
2023-07-14 13:48:53 +0530

    add hindi translation in index.js

--- app/i18n/index.js
+++ app/i18n/index.js
@@ -1,36 +1,35 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import moment from 'moment';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import moment from "moment";
 
 const languages = {
-    de: { translation: require('./lang/de.json') },
-    en: { translation: require('./lang/en.json') },
-    es: { translation: require('./lang/es.json') },
-    fr: { translation: require('./lang/fr.json') },
-    gl: { translation: require('./lang/gl.json') },
-    hr: { translation: require('./lang/hr.json') },
-    hu: { translation: require('./lang/hu.json') },
-    it: { translation: require('./lang/it.json') },
-    nl: { translation: require('./lang/nl.json') },
-    pt: { translation: require('./lang/pt-br.json') },
-    ru: { translation: require('./lang/ru.json') },
-    sq: { translation: require('./lang/sq.json') },
-    'zh-CN': { translation: require('./lang/zh-CN.json') },
-    'zh-TW': { translation: require('./lang/zh-TW.json') }
+  de: { translation: require("./lang/de.json") },
+  en: { translation: require("./lang/en.json") },
+  es: { translation: require("./lang/es.json") },
+  fr: { translation: require("./lang/fr.json") },
+  gl: { translation: require("./lang/gl.json") },
+  hi: { translation: require("./lang/hi.json") },
+  hr: { translation: require("./lang/hr.json") },
+  hu: { translation: require("./lang/hu.json") },
+  it: { translation: require("./lang/it.json") },
+  nl: { translation: require("./lang/nl.json") },
+  pt: { translation: require("./lang/pt-br.json") },
+  ru: { translation: require("./lang/ru.json") },
+  sq: { translation: require("./lang/sq.json") },
+  "zh-CN": { translation: require("./lang/zh-CN.json") },
+  "zh-TW": { translation: require("./lang/zh-TW.json") },
 };
 
 const detectedLocale = navigator.language;
 
-i18n
-    .use(initReactI18next)
-    .init({
-        resources: languages,
-        lng: detectedLocale,
-        fallbackLng: 'en',
-        interpolation: {
-            escapeValue: false // not needed for react as it escapes by default
-        }
-    });
+i18n.use(initReactI18next).init({
+  resources: languages,
+  lng: detectedLocale,
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false, // not needed for react as it escapes by default
+  },
+});
 
 moment.locale(detectedLocale);
 

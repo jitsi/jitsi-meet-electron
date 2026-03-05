b5b20ec3cd1d59cd0505790edfc63dbc442d271d
Shivam Sharma <shivam14052004@gmail.com>
2026-03-06 01:52:11 +0530

    feat(two-window): redesigned Recent Meetings UI and two-window layout

--- app/features/welcome/styled/Body.js
+++ app/features/welcome/styled/Body.js
@@ -3,9 +3,7 @@ import styled from 'styled-components';
 
 export default styled.div`
     margin: 0 12.5%;
-    overflow: scroll;
-
-    ::-webkit-scrollbar {
-        display: none;
-    }
+    flex: 1;
+    min-height: 0;
+    overflow: hidden;
 `;
